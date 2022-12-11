0caceecbc5e12a718b4801cc2dd7fb885ceacf49
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-12-11 16:01:29 +0200

    saving our products to our session

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
     public function cart()
     {
+        //var_dump(session('cartItems'));
         return view('cart.index');
     }
+
+
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cartItems = session()->get('cartItems', []);
+
+        if(isset($cartItems[$id])){
+            $cartItems[$id]['quantity']++;
+        }
+        else {
+            $cartItems[$id] = [
+                'image_path' => $product->image_path,
+                'name' => $product->name,
+                'details' => $product->details,
+                'price' => $product->price,
+                'quantity' => 1
+            ];
+        }
+        session()->put('cartItems', $cartItems);
+
+        return redirect()->back()->with('success', 'Product added to cart');
+    }
+
 }
